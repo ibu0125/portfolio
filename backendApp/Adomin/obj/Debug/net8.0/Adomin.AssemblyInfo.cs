6e73cf5e1573d1d87638e2fa29a696dd15ca5373
ibu0125 <ibu01257248@gmail.com>
2024-10-09 01:37:45 +0900

    データ消えたため再構築

--- backendApp/Adomin/obj/Debug/net8.0/Adomin.AssemblyInfo.cs
+++ backendApp/Adomin/obj/Debug/net8.0/Adomin.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Adomin")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+29b9216c7feab106a18e833b3b9664486af17a81")]
 [assembly: System.Reflection.AssemblyProductAttribute("Adomin")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Adomin")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- backendApp/FormBackend/FormBackend/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace FormBackend.Models {
-    public class ErrorViewModel {
-        public string? RequestId {
-            get; set;
-        }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
